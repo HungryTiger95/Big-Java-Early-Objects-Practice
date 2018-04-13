6faa11eea5d8952e96ad30a009cd712f1cb695da
HungryTiger95 <35184571+HungryTiger95@users.noreply.github.com>
2018-04-13 11:20:40 -0400

    Add files via upload

---
+++ Chapter 1/LettersName.java
@@ -0,0 +1,19 @@
+/**
+ * E1.6
+ * 
+ * Write a program that prints your name in large letters
+ * 
+ * @author Mayuresh
+ *
+ */
+public class LettersName
+{
+	public static void main(String[] args)
+	{
+		System.out.println("**    ** *****");
+		System.out.println("* *  * * *   *");
+		System.out.println("*  **  * *****");
+		System.out.println("*      * *");
+		System.out.println("*      * *");
+	}
+}
---
+++ Chapter 1/ThreeItems.java
@@ -0,0 +1,17 @@
+/**
+ * E1.11
+ * 
+ * Write a program that prints three items on three separate lines
+ * 
+ * @author Mayuresh
+ *
+ */
+public class ThreeItems
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello Brother");
+		System.out.println("Once Upon A Time in Mumbai");
+		System.out.println("Ram-Leela");
+	}
+}
