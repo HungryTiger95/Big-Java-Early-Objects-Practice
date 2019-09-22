5c5f0e7cbd9a804396523a72e9514f1da5ac95ae
HungryTiger95 <35184571+HungryTiger95@users.noreply.github.com>
2019-09-22 14:41:38 -0400

    Add files via upload

---
+++ Chapter 1/Practice Exercises/PrintSum.java
@@ -0,0 +1,17 @@
+/**
+ * E1.2
+ * 
+ * Write a program that prints the sum of the first ten positve integers
+ * 
+ * @author Mayuresh
+ *
+ */
+
+public class PrintSum
+{
+	public static void main (String[] args)
+	{
+		int sum = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10;
+		System.out.println("Sum of first ten positive integers: " + sum);
+	}
+}
